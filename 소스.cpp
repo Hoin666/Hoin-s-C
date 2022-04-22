43ab6204a48a0d69695ac16e097c5642e5a8a760
Hoin666 <101854332+Hoin666@users.noreply.github.com>
2022-04-22 13:30:11 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -1,36 +1,85 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-
-
-double interval(double a, double b);
+void swap(int *A,int  *B){
+	int temp = *A;
+	*A = *B;
+	*B = temp;
+}
 int main(void) {
-	srand((unsigned)time(NULL));
-	char buffer[1000] = { 0 , };
-	FILE* fp = fopen("circle.txt", "w");
-	double x, y;
-	for (int i = 1; i <= 100; i++){
-		char temp1[10], temp2[10];
-		x = interval(0.0, 1.0);
-		y = interval(0.0, 1.0);
-		sprintf(temp1, "%lf", x);
-		sprintf(temp2, "%lf", y);
-		fputs(temp1, fp);
-		fputs(" ", fp);
-		fputs(temp2, fp);
-		fputs("\n", fp);
-		
-	}
-	fclose(fp);
+	int a = 3, b = 4;
+	swap(&a, &b);
+	printf("%d  %d", a, b);
+	return 0;
 }
-double interval(double a, double b) {
-	double random;
-	while (true) {
-		random =a + (double)(rand()%1000) / 1000;
-		if (random > a and random < b) {
-			break;
-		}
-	}
-	return random;
+/*
+주관식
+Q1. 2개의 정수를 서로 교환하는 함수 알고리즘을 슈도코드로 작성해라.
+
+A : void swap(*A, *B){
+	temp <- *A;
+	*A <- *B;
+	*B <- temp;
+Q2. 다음 코드의 시간 복잡도는? (이때 n = 2^t)
+ for(int i = 1; i < n; i *= 2){
+	printf("Hello");
 }
+
+A : 총 t번의 연산이 이루어지고 반복문이 종료되므로,
+	시간복잡도 = t = log2 n 이다.
+
+Q3. O(n^2)의 시간복잡도를 가지는 알고리즘에서 입력의 개수가 2배가 되면 실행시간은 얼만큼 증가하는가?
+
+A : n = t일때 t^2 만큼 시간이 걸리고
+    n = 2t일때 4t^2 만큼 시간이 걸리므로 4배만큼 증가한다.
+
+Q4. int a[10][20]에서 배열이 차지하는 메모리 공간의 크기는 얼마인가?
+
+A : int형 변수의 크기는 4byte이고 개수가 10 x 20 개 이므로 800바이트이다.
+
+Q5.
+
+#define max_size 100
+
+typedef struct Stacktype {
+	int stack[max_size];
+	int top;
+}Stacktype;
+
+void Stack_init(Stacktype* s) {
+	s->top = -1;
+}
+
+void Stack_push(Stacktype* s, int item) {
+	s->stack[++(s->top)] = item;
+	return;
+}
+
+bool is_empty(Stacktype* s) {
+	return s->top == -1;
+}
+
+int Stack_pop(Stacktype* s) {
+	int t = s->stack[s->top];
+	s->stack[s->top] = 0;
+	s->top--;
+	return t;
+}
+
+int main(void) {
+	int n, temp;
+	Stacktype s;
+	Stack_init(&s);
+
+	printf("정수 배열의 크기 : ");
+	scanf_s("%d", &n);
+	printf("정수를 입력하시오 : ");
+	for (int i = 0; i < n; i++) {
+		scanf_s("%d", &temp);
+		Stack_push(&s, temp);
+	}
+	printf("반전된 정수 배열 : ");
+	for (int i = 0; i < n; i++) {
+		printf("%d ", Stack_pop(&s));
+	}
+	return 0;
+}
+*/
